9ad8207fbb27bcb619d448dad4836b29efa477e4
jackytruongpro <jacky-t@hotmail.fr>
2024-01-10 00:51:51 +0100

    Resolve some warnings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import adapter from '@sveltejs/adapter-static';
+
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
@@ -8,6 +10,12 @@ export default {
         lato: ['"Lato"', "sans-serif"]
       },
     }
+  },
+  kit: {
+    adapter: adapter(),
+    files: {
+      assets: 'static'
+    },
   },
   plugins: []
 };
